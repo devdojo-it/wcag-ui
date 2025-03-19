5abdbae7893ec441d902ac08d1bf4014c877198d
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-03-19 23:39:35 +0100

    feat(docs): add pages for foundations and components

---
+++ packages/components/dialog/lib/dialog.events.js
@@ -0,0 +1,8 @@
+export default {
+  // click: function (e) {
+  //   console.log("button clicked", this.textContent);
+  // },
+  // focus: function (e) {
+  //   console.log("button focused", this.textContent);
+  // },
+};
--- packages/components/dialog/lib/dialog.js
+++ packages/components/dialog/lib/dialog.js
@@ -1,7 +1,39 @@
-'use strict';
+import { componentDecorator } from '@wcag-ui/core';
+import { DOM } from '@wcag-ui/dom';
 
-module.exports = dialog;
+import './styles/dialog.css';
 
-function dialog() {
-  return 'Hello from dialog';
+import attributes from './dialog.attributes';
+import events from './dialog.events';
+
+/**
+ * wcagUI Dialog class
+ *
+ * @export
+ * @class Dialog
+ * @extends {HTMLDialogElement}
+ */
+export class Dialog extends HTMLDialogElement {
+  static name = 'wcag-dialog';
+  static extends = 'dialog';
+  static attributes = attributes;
+  static events = events;
+
+  /**
+   * static initialization
+   *
+   * @static
+   * @memberof Dialog
+   */
+  static {
+    componentDecorator('Dialog', Dialog);
+  }
+
+  constructor() {
+    super();
+
+    this.#initialize();
+  }
+
+  #initialize() {}
 }
