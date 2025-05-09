1f53ac15a903cfd0c0982ed49fc83fa0fecec90f
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-05-09 21:44:20 +0200

    feat(accordion): add accordion component main behaviors

--- packages/components/accordion/lib/accordion.attributes.js
+++ packages/components/accordion/lib/accordion.attributes.js
@@ -1,10 +1,7 @@
-import { events } from "@wcag-ui/core";
+import { events } from '@wcag-ui/core';
 
 export default {
-  // open: function (oldValue, newValue) {
-  //   const state = newValue === null ? "close" : "open";
-  //   this.setAttribute("aria-expanded", (state === "open").toString());
-
-  //   events.dispatchComponentEvent.call(this, "toggle", { state });
-  // },
+  name: function () {
+    this.update();
+  },
 };
