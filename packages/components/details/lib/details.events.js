e98b46aaea1f142709af60e43102a978c882fdc2
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-05-09 21:44:20 +0200

    feat(accordion): add accordion component main behaviors

--- packages/components/details/lib/details.events.js
+++ packages/components/details/lib/details.events.js
@@ -1,8 +1,1 @@
-export default {
-  // click: function (e) {
-  //   console.log("button clicked", this.textContent);
-  // },
-  // focus: function (e) {
-  //   console.log("button focused", this.textContent);
-  // },
-};
+export default {};
