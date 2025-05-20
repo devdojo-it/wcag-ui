f7b4f05b96f8bd3ef00aa360026f8f21409029c1
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-05-20 13:11:44 +0200

    feat(tooltip): add empty component code structure

--- packages/components/tooltip/lib/tooltip.js
+++ packages/components/tooltip/lib/tooltip.js
@@ -1,7 +1,26 @@
-'use strict';
+import './styles/tooltip.css';
 
-module.exports = tooltip;
+import { componentDecorator, error } from '@wcag-ui/core';
+import { DOM } from '@wcag-ui/dom';
 
-function tooltip() {
-  return 'Hello from tooltip';
+import attributes from './tooltip.attributes';
+import events from './tooltip.events';
+
+export class Tooltip extends HTMLElement {
+  static name = 'wcag-tooltip';
+  static extends = 'span';
+  static attributes = attributes;
+  static events = events;
+
+  static {
+    componentDecorator('Tooltip', Tooltip);
+  }
+
+  constructor() {
+    super();
+
+    this.#initialize();
+  }
+
+  #initialize() {}
 }
