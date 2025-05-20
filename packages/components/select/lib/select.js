3b51ee91f398dc33758bf7910126ce6e2e2b7aa5
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-05-20 13:29:45 +0200

    feat(select): add empty component code structure

--- packages/components/select/lib/select.js
+++ packages/components/select/lib/select.js
@@ -1,7 +1,26 @@
-'use strict';
+import './styles/select.css';
 
-module.exports = select;
+import { componentDecorator, error } from '@wcag-ui/core';
+import { DOM } from '@wcag-ui/dom';
 
-function select() {
-  return 'Hello from select';
+import attributes from './select.attributes';
+import events from './select.events';
+
+export class Select extends HTMLElement {
+  static name = 'wcag-select';
+  static extends = 'span';
+  static attributes = attributes;
+  static events = events;
+
+  static {
+    componentDecorator('Select', Select);
+  }
+
+  constructor() {
+    super();
+
+    this.#initialize();
+  }
+
+  #initialize() {}
 }
