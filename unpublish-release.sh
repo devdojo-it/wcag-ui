665de8bb215daebfdb1043d32baad0da2cee5723
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-05-20 12:17:01 +0200

    docs(packages): add authors and contributors, add description to all packages

---
+++ unpublish-release.sh
@@ -0,0 +1,12 @@
+npm unpublish @wcag-ui/accordion@0.0.8 --force &&
+npm unpublish @wcag-ui/button@0.0.8 --force &&
+npm unpublish @wcag-ui/checkbox@0.0.8 --force &&
+npm unpublish @wcag-ui/core@0.0.8 --force &&
+npm unpublish @wcag-ui/details@0.0.8 --force &&
+npm unpublish @wcag-ui/grid-system@0.0.8 --force &&
+npm unpublish @wcag-ui/iconography@0.0.8 --force &&
+npm unpublish @wcag-ui/input@0.0.8 --force &&
+npm unpublish @wcag-ui/radio@0.0.8 --force &&
+npm unpublish @wcag-ui/scroll-spy@0.0.8 --force &&
+npm unpublish @wcag-ui/switch@0.0.8 --force &&
+npm unpublish @wcag-ui/typography@0.0.8 --force
