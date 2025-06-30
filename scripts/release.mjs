f6f907ad2dfd53631d0d007430f219fdc1abd7d1
Emiliano Pisu <75838944+pixu1980@users.noreply.github.com>
2025-06-30 14:33:20 +0200

    build(release): update release scripts for updating dependencies with workspace prefix

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -113,25 +113,7 @@ if (fs.existsSync(rootPkgPath)) {
 }
 
 /* ------------------------------------------------------------------------ */
-/* 7. Updates the changelog file from lastTagOrCommit to HEAD               */
-/* ------------------------------------------------------------------------ */
-execSync(`changelog -t ${lastTagOrCommit}..HEAD -x rel,build,chore`, { stdio: "inherit" });
-
-// /* ------------------------------------------------------------------------ */
-// /* 8. Pushes all the updated files in a chore(release): commit              */
-// /* ------------------------------------------------------------------------ */
-execSync("git add .", { stdio: "inherit" });
-execSync(`git commit -m "chore(release): v${next}"`, { stdio: "inherit" });
-execSync("git push", { stdio: "inherit" });
-
-// /* ------------------------------------------------------------------------ */
-// /* 9. Generates and pushes the {next} semver tag                            */
-// /* ------------------------------------------------------------------------ */
-execSync(`git tag v${next}`);
-execSync("git push --tags", { stdio: "inherit" });
-
-/* ------------------------------------------------------------------------ */
-/* 10. Publishes the packages into the configured registry (default: npm)   */
+/* 7. Publishes the packages into the configured registry (default: npm)   */
 /* ------------------------------------------------------------------------ */
 for (const packageJson of packageJsonPathsRemap) {
   if (path.dirname(packageJson.path).includes("packages")) {
@@ -139,6 +121,28 @@ for (const packageJson of packageJsonPathsRemap) {
   }
 }
 
+/* ------------------------------------------------------------------------ */
+/* 8. Updates the changelog file from lastTagOrCommit to HEAD               */
+/* ------------------------------------------------------------------------ */
+execSync(`changelog -t ${lastTagOrCommit}..HEAD -x rel,build,chore`, { stdio: "inherit" });
+
+/* ------------------------------------------------------------------------ */
+/* 9. Pushes all the updated files in a chore(release): commit              */
+/* ------------------------------------------------------------------------ */
+execSync("git add .", { stdio: "inherit" });
+execSync(`git commit -m "chore(release): v${next}"`, { stdio: "inherit" });
+execSync("git push", { stdio: "inherit" });
+
+/* ------------------------------------------------------------------------ */
+/* 10. Generates and pushes the {next} semver tag                            */
+/* ------------------------------------------------------------------------ */
+execSync(`git tag v${next}`);
+execSync("git push --tags", { stdio: "inherit" });
+
+/* ------------------------------------------------------------------------ */
+/* 11. Resets the internal dependencies versions with `workspace:^` prefix  */
+/*     in order to make it work with pnpm                                   */
+/* ------------------------------------------------------------------------ */
 for (const packageJson of packageJsonPathsRemap) {
   const pkg = JSON.parse(fs.readFileSync(packageJson.path, "utf8"));
 
@@ -161,5 +165,5 @@ for (const localPackage of localPackages) {
 fs.writeFileSync(rootPkgPath, JSON.stringify(rootPkg, null, 2) + "\n");
 
 execSync("git add .", { stdio: "inherit" });
-execSync("git commit --amend --no-edit", { stdio: "inherit" });
-execSync("git push", { stdio: "inherit" });
+execSync(`git commit --ament --no-edit`, { stdio: "inherit" });
+execSync("git push -f", { stdio: "inherit" });
